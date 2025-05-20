9ae61bc9c99c520624ad2d91d97bdb142c663f7a
Omkar Deshmukh <deshmukhomkar144@gmail.com>
2025-05-20 13:00:42 +0530

    folder strcuture added

---
+++ README.md
@@ -0,0 +1,11 @@
+
+### STEPS TO PERFORM THE PROJECT
+
+
+### 1  clone the Repository
+
+### 2  conda create -n medicalbot python=3.10 -y
+
+### 3 conda activate medicalbot
+
+### 4 pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Generative AI medical bot',
+    version='0.0.0',
+    author='Omkar Deshmukh',
+    author_email='omkardeshmukh1403@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
